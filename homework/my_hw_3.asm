34fb7d82d8c5ed907b5b41fc847b9de8b912c191
Vlad <vlad.lopatinsky@gmail.com>
2024-10-08 14:01:43 -0700

    Working on HW3

---
+++ homework/my_hw_3.asm
@@ -0,0 +1,17 @@
+
+.data
+
+	# String literals used in the program
+	newline: .asciiz "\n"
+	prompt_y: .asciiz "Enter y: "
+	prompt_x: .asciiz "Enter x: "
+	str_arr_open: .asciiz "arr["
+	str_close_open: .asciiz "]["
+	str_colon_space: .asciiz "]: "
+	str_address: .asciiz "Address: "
+	
+	#num of collumns
+	n_cols: .word 5
+
+.text
+
